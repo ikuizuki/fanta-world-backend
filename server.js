d768c539aa50db91731b76fe399c2965b796b426
ikuizuki <ikuizukidong@gmail.com>
2026-01-27 22:55:30 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
 app.use(express.json()); // QUAN TRỌNG
+
+app.use(
+  cors({
+    origin: "https://fanta-world-gamma.vercel.app",
+  }),
+);
 
 // test api
 app.get("/api/test", (req, res) => {
