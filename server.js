a3ce7359cdde2692c5c2458a8567ad8ec2631536
ikuizuki <ikuizukidong@gmail.com>
2026-01-27 22:45:50 +0700

    update

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
+app.use(express.json()); // QUAN TRỌNG
 
+// test api
 app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from backend 👋" });
 });
+//trả về vị trí
+app.get("/api/player/position", (req, res) => {
+  res.json(playerPosition);
+});
+// lưu vị trí player (tạm thời)
+let playerPosition = { x: 0, y: 0 };
+
+app.post("/api/player/position", (req, res) => {
+  const { x, y } = req.body;
+
+  if (typeof x !== "number" || typeof y !== "number") {
+    return res.status(400).json({ error: "Invalid position" });
+  }
+
+  playerPosition.x = x;
+  playerPosition.y = y;
+
+  console.log("Player position:", playerPosition);
+
+  res.json({ status: "ok" });
+});
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
