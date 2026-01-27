d728c2501e6fc6487ff0207a4c2e069c8bba9b96
ikuizuki <ikuizukidong@gmail.com>
2026-01-27 17:45:44 +0700

    update

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-// API test
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend" });
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello from backend 👋" });
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
