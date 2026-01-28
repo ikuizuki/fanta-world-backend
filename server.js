9b3c0284ab5acbaf017ce3026556571d9c35f152
ikuizuki <ikuizukidong@gmail.com>
2026-01-28 18:09:12 +0700

    update

--- server.js
+++ server.js
@@ -21,7 +21,36 @@ let playerPosition = { x: 0, y: 0 };
 app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from backend 👋" });
 });
+//lấy vị trí
+const players = {}; // tạm thời (sau này thay bằng DB)
+app.get("/api/player/:playerId", (req, res) => {
+  const { playerId } = req.params;
 
+  if (!players[playerId]) {
+    players[playerId] = {
+      x: 100,
+      y: 100,
+    };
+  }
+  playerPosition = players[playerId];
+  console.log("Player position:", playerPosition);
+
+  res.json(players[playerId]);
+});
+/* lưu vị trí */
+app.post("/api/player/:playerId", (req, res) => {
+  const { playerId } = req.params;
+  const { x, y } = req.body;
+
+  if (typeof x !== "number" || typeof y !== "number") {
+    return res.status(400).json({ error: "Invalid position" });
+  }
+  playerPosition = { x, y };
+  players[playerId] = playerPosition;
+  console.log("Player position:", playerPosition);
+
+  res.json({ status: "ok" });
+});
 /* lấy vị trí */
 app.get("/api/player/position", (req, res) => {
   res.json(playerPosition);
