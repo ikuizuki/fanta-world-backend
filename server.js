1110dd68d4bf79233370b4eb4fa2fcca5321328d
ikuizuki <ikuizukidong@gmail.com>
2026-02-16 18:23:38 +0700

    update

--- server.js
+++ server.js
@@ -18,35 +18,68 @@ app.use(express.json());
 app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from backend 👋" });
 });
-//lấy vị trí
-const players = {}; // tạm thời (sau này thay bằng DB)
-app.get("/api/player/:playerId", (req, res) => {
-  const { playerId } = req.params;
+//database
+const db_password = process.env.DB_PASSWORD;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-  if (!players[playerId]) {
-    players[playerId] = {
-      x: 100,
-      y: 100,
-    };
+const uri =
+  "mongodb+srv://ikuizukidong_db_user:" +
+  db_password +
+  "@cluster0.5djfxz7.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let playersCollection; //chứa database
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("✅ Connected to MongoDB");
+
+    const db = client.db("fanta_world"); // tên database của bạn
+    playersCollection = db.collection("players"); // collection players (database)
+  } catch (error) {
+    console.error("❌ DB Connection Error:", error);
   }
-  playerPosition = players[playerId];
-  console.log("Player position:", playerPosition);
+}
+connectDB();
 
-  res.json(players[playerId]);
+//lấy vị trí
+app.get("/api/player/:playerId", async (req, res) => {
+  const { playerId } = req.params;
+  if (!playersCollection) {
+    return res.status(500).json({ error: "Database not connected" });
+  }
+  let player = await playersCollection.findOne({ playerId });
+  if (!player) {
+    player = { playerId, x: 100, y: 100 };
+    await playersCollection.insertOne(player);
+  }
+
+  res.json({ x: player.x, y: player.y });
 });
 /* lưu vị trí */
-app.post("/api/player/:playerId", (req, res) => {
+app.post("/api/player/:playerId", async (req, res) => {
   const { playerId } = req.params;
   const { x, y } = req.body;
-
+  if (!playersCollection) {
+    return res.status(500).json({ error: "Database not connected" });
+  }
   if (typeof x !== "number" || typeof y !== "number") {
     return res.status(400).json({ error: "Invalid position" });
   }
-  playerPosition = { x, y };
-  players[playerId] = playerPosition;
-  console.log("Player position:", playerPosition);
-
-  res.json({ status: "ok" });
+  await playersCollection.updateOne(
+    { playerId },
+    { $set: { x, y } },
+    { upsert: true },
+  );
+  res.json({ status: "saved" });
 });
 
 const PORT = process.env.PORT || 3000;
